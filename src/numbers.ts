babd536e43ac263ee1d85bd007d22fe2d9a963d9
Chris Pikul <chris@workmaple.com>
2023-07-27 12:23:30 +0300

    config values octal numbers binary numbers refactor rand

--- src/numbers.ts
+++ src/numbers.ts
@@ -1,4 +1,5 @@
 import { parseRange, withDigits } from './utils';
+import { getConfigValue } from './vscode';
 
 function rand(mult: number): number {
     return Math.random() * mult;
@@ -88,17 +89,57 @@ export function randomSignedShort(): string {
 }
 
 export function randomHexInteger16(): string {
-    return `0x${Math.trunc(rand(0xffff)).toString(16).toUpperCase()}`;
+    const prefix = getConfigValue<string>('numbers.hexPrefix');
+    return `${prefix}${Math.trunc(rand(0xffff)).toString(16).toUpperCase()}`;
 }
 
 export function randomHexInteger24(): string {
-    return `0x${Math.trunc(rand(0xffffff)).toString(16).toUpperCase()}`;
+    const prefix = getConfigValue<string>('numbers.hexPrefix');
+    return `${prefix}${Math.trunc(rand(0xffffff)).toString(16).toUpperCase()}`;
 }
 
 export function randomHexInteger32(): string {
-    return `0x${Math.trunc(rand(0xffffffff)).toString(16).toUpperCase()}`;
+    const prefix = getConfigValue<string>('numbers.hexPrefix');
+    return `${prefix}${Math.trunc(rand(0xffffffff))
+        .toString(16)
+        .toUpperCase()}`;
 }
 
 export function randomDigit(): string {
     return Math.trunc(Math.random() * 10).toString();
+}
+
+export function randomOctalInteger16(): string {
+    const prefix = getConfigValue<string>('numbers.octalPrefix');
+    return `${prefix}${Math.trunc(rand(0xffff)).toString(8)}`;
+}
+
+export function randomOctalInteger24(): string {
+    const prefix = getConfigValue<string>('numbers.octalPrefix');
+    return `${prefix}${Math.trunc(rand(0xffffff)).toString(8)}`;
+}
+
+export function randomOctalInteger32(): string {
+    const prefix = getConfigValue<string>('numbers.octalPrefix');
+    return `${prefix}${Math.trunc(rand(0xffffffff)).toString(8)}`;
+}
+
+export function randomBinaryInteger8(): string {
+    const prefix = getConfigValue<string>('numbers.binaryPrefix');
+    return `${prefix}${Math.trunc(rand(0xff)).toString(2)}`;
+}
+
+export function randomBinaryInteger16(): string {
+    const prefix = getConfigValue<string>('numbers.binaryPrefix');
+    return `${prefix}${Math.trunc(rand(0xffff)).toString(2)}`;
+}
+
+export function randomBinaryInteger24(): string {
+    const prefix = getConfigValue<string>('numbers.binaryPrefix');
+    return `${prefix}${Math.trunc(rand(0xffffff)).toString(2)}`;
+}
+
+export function randomBinaryInteger32(): string {
+    const prefix = getConfigValue<string>('numbers.binaryPrefix');
+    return `${prefix}${Math.trunc(rand(0xffffffff)).toString(2)}`;
 }
--- src/vscode.ts
+++ src/vscode.ts
@@ -198,3 +198,31 @@ export function registerCommand(
         registerGeneratorCommand(context, cmd);
     else console.error(`Invalid command "${cmd.key}"!`);
 }
+
+/**
+ * Get a configuration value using the given setting key.
+ * Searches globally within the workspace.
+ *
+ * @param key Workspace setting key
+ * @returns Value at that key
+ */
+export function getGlobalConfigValue<T = unknown>(key: string): T {
+    const lastDot = key.lastIndexOf('.');
+
+    const section = key.substring(0, lastDot);
+    const prop = key.substring(lastDot + 1);
+
+    return vscode.workspace.getConfiguration(section).get(prop) as T;
+}
+
+/**
+ * Get a configuration value within the Mockingbird settings using the
+ * given sub-key. This will prefix the key for you.
+ *
+ * @see {@link getGlobalConfigValue} for the real implementation
+ * @param key Setting key within Mockingbird
+ * @returns Value at that key
+ */
+export function getConfigValue<T = unknown>(key: string): T {
+    return getGlobalConfigValue<T>(`mockingbird.${key}`);
+}
